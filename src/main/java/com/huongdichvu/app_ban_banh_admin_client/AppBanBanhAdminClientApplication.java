5386c296c894d1fd37cf7742711957e46cc16577
TranDo25 <73243952+TranDo25@users.noreply.github.com>
2023-05-10 16:29:28 +0700

    first commit

---
+++ src/main/java/com/huongdichvu/app_ban_banh_admin_client/AppBanBanhAdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.huongdichvu.app_ban_banh_admin_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBanBanhAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBanBanhAdminClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huongdichvu/app_ban_banh_admin_client/AppBanBanhAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huongdichvu.app_ban_banh_admin_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBanBanhAdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
